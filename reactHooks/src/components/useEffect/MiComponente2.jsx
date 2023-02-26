62ad7bb235417ad53f74fd67cacb4d024004c954
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-02-26 20:48:10 -0300

    useEffect para cambios de un solo estado

--- reactHooks/src/components/useEffect/MiComponente2.jsx
+++ reactHooks/src/components/useEffect/MiComponente2.jsx
@@ -4,17 +4,23 @@ const MiComponente2 = () => {
 
     const [nombre, setNombre] = useState("Juan");
 
+    const [nombre1, setNombre1] = useState("Juan");
+
     useEffect(() => {
         console.log(nombre);
-    }), [];
+    }, [nombre]);
 
     const handleClick = () => {
         nombre === "Juan" ? setNombre("Pedro") : setNombre("Juan");
     }
 
+    const handleClick1 = () => {
+        nombre1 === "Juan" ? setNombre1("Pedro") : setNombre1("Juan");
+    }
+
   return <>
     <h1 onClick={handleClick}>useEffect</h1>
-
+    <h2 onClick={handleClick1}>Sin efecto</h2>
   </>;
 };
 
