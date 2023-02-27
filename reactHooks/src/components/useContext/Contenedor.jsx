9db9fb7b34009faad1a066f19a02ebdb917c738a
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-02-27 12:01:31 -0300

    useContext y useReducer

--- reactHooks/src/components/useContext/Contenedor.jsx
+++ reactHooks/src/components/useContext/Contenedor.jsx
@@ -1,11 +1,34 @@
-import React, {createContext} from 'react'
+import React, {createContext, useReducer} from 'react'
 import MiComponente4 from './MiComponente4'
 
 const Contenedor = () => {
+
+  const data = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 30,
+    status: 0
+  }
+
+  const reducer = (state, action) =>{
+    switch(action.type){
+      case "seleccionar":
+        return {...state, status: 1}
+      case "deseleccionar":
+        return {...state, status: 0}
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, data )
   return (<>
-    <MiContexto.Provider value={{nombre: 'Juan', apellido: 'Perez'}}>
+    <MiContexto.Provider value={{state, dispatch}}>
     <MiComponente4/>
     </MiContexto.Provider>
+
+    <button onClick={() => {dispatch({type: "seleccionar"})}}>Seleccionar</button>
+    <button onClick={() => {dispatch({type: "deseleccionar"})}}>Deseleccionar</button>
   </>
   )
 }
--- reactHooks/src/components/useContext/MiComponente4.jsx
+++ reactHooks/src/components/useContext/MiComponente4.jsx
@@ -1,16 +1,27 @@
-import React, {useContext} from 'react'
-import {MiContexto} from './Contenedor'
+import React, { useContext } from "react";
+import { MiContexto } from "./Contenedor";
 
 const MiComponente4 = () => {
+  const { state } = useContext(MiContexto);
 
-    const {nombre, apellido} = useContext(MiContexto)
+  // console.log({ ...state });
 
-  return (<>
-    <h3>MiComponente4</h3>
-    <p>Nombre: {nombre}</p>
-    <p>Apellido: {apellido}</p>
-  </>
-  )
-}
+  return (
+    <>
+      <h3>MiComponente4</h3>
+      <div>
+        <p>Nombre: {state.nombre}</p>
+        <p>Apellido: {state.apellido}</p>
+        <p>Edad: {state.edad}</p>
 
-export default MiComponente4
+        {state.status === 1 ? (
+          <p>Seleccionado</p>
+        ) : (
+          <p>No seleccionado</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default MiComponente4;
